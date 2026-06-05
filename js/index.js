1e2246d031597c7178863842d0c13eb9586cb92d
Bot <268117545+jcxyis-bot[bot]@users.noreply.github.com>
2026-06-05 16:28:55 +0000

    Auto update -> Match https://github.com/AzurLaneTools/AzurLaneLuaScripts/commit/fba1461860c78498d93a231be19020c83173a1e6 7866e29d91d2268566e6402d8a59c9cb8d07027d

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ const typeNames = {
     "1": "Main Story",
     "2": "Event",
     "3": "Character Story",
+    "4": "Hall of Fame",
     "non-archived": "Non Archived"
 };
 
@@ -17,8 +18,7 @@ const subtypeNames = {
     "2": { // Subtypes for Type 2 (Event)
         "1": "Event (EX)",
         "2": "Special (SP)",
-        "3": "Permanent (Daily Life)",
-        "4": "Hall of Fame",
+        "3": "Permanent (Daily Life)",        
     }
 };
 
